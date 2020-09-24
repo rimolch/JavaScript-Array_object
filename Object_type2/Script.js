4c491b94d3ca4370502bd4846cc653a1c90fd753
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-09-24 14:56:46 +0600

    Create Script.js

---
+++ Object_type2/Script.js
@@ -0,0 +1,10 @@
+var names = [ " Yaakov ", " John "," Mark "];
+var myobj = {
+         name :" Yaakov ",
+         Course : " HTML ,CSS & JS ",
+         platform: " Coursera "
+         };
+for(var prop in myobj){
+console.log( prop + ":" + myobj[prop]);
+}
+      
