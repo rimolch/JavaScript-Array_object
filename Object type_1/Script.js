71b306921f2a9ffb01d330c804fa651c8a735689
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-09-24 14:45:39 +0600

    Create Script.js

---
+++ Object type_1/Script.js
@@ -0,0 +1,5 @@
+var names = [ " Yaakov ", " John "," Mark "];
+names[100] = " Chakein ";//returns  Hello Chakein 100 times
+for (var i = 0; i<names.length; i++){
+console.log(" Hello " + names[i]);
+}
